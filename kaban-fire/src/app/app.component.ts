8154f0c69027224840f0829491286a3ec1bf9e8c
ranoob <whyfck@gmail.com>
2023-08-24 19:05:47 -0300

    task crud logic and drag-drop in/between columns

--- kaban-fire/src/app/app.component.ts
+++ kaban-fire/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog'
+import { TaskDialogResult } from './task-dialog/task-dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kaban-fire';
+  todo: Task[] = [
+    {
+      title: 'Buy milk',
+      description: 'Go to the store and buy milk'
+    },
+    {
+      title: 'Create a Kanban app',
+      description: 'Using Firebase and Angular create a Kanban app!'
+    }
+  ]
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  constructor(private dialog: MatDialog) { }
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: `${270 / 16}rem`,
+      data: {
+        task: {}
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult | undefined) => {
+      if (!result) return
+
+      this.todo.push(result.task)
+    })
+  }
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: `${270 / 16}rem`,
+      data: {
+        task,
+        enableDelete: true
+      }
+    })
+
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult | undefined) => {
+      if (!result) return
+
+      const dataList = this[list]
+      const taskIndex = dataList.indexOf(task)
+
+      if (result.delete) {
+        dataList.splice(taskIndex, 1)
+      } else {
+        dataList[taskIndex] = task
+      }
+    })
+  }
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (!event.container.data || !event.previousContainer.data) return
+
+    if (event.previousContainer === event.container) {
+      return moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+    }
+
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    )
+  }
 }
