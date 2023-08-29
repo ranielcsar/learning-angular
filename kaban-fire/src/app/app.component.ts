9bd53feaf3cec059c37afd55585a0c56d3df88ea
ranoob <whyfck@gmail.com>
2023-08-29 09:00:52 -0300

    added firebase collections and update crud logic

--- kaban-fire/src/app/app.component.ts
+++ kaban-fire/src/app/app.component.ts
@@ -1,44 +1,62 @@
-import { Component } from '@angular/core';
-import { Task } from './task/task';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component } from '@angular/core'
+import { Task } from './task/task'
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop'
 import { MatDialog } from '@angular/material/dialog'
-import { TaskDialogResult } from './task-dialog/task-dialog';
-import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { TaskDialogResult } from './task-dialog/task-dialog'
+import { TaskDialogComponent } from './task-dialog/task-dialog.component'
+import {
+  Firestore,
+  collectionData,
+  collection,
+  runTransaction,
+  deleteDoc,
+  addDoc,
+  updateDoc,
+  documentId,
+  doc
+} from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  todo: Task[] = [
-    {
-      title: 'Buy milk',
-      description: 'Go to the store and buy milk'
-    },
-    {
-      title: 'Create a Kanban app',
-      description: 'Using Firebase and Angular create a Kanban app!'
-    }
-  ]
-  inProgress: Task[] = [];
-  done: Task[] = [];
+  todo: Observable<Task[]>
+  inProgress: Observable<Task[]>
+  done: Observable<Task[]>
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private store: Firestore) {
+    const todos = collection(this.store, 'todo')
+    const inProgress = collection(this.store, 'inProgress')
+    const done = collection(this.store, 'done')
+
+    this.todo = collectionData(todos, { idField: 'id' }) as Observable<Task[]>
+    this.inProgress = collectionData(inProgress, { idField: 'id' }) as Observable<Task[]>
+    this.done = collectionData(done, { idField: 'id' }) as Observable<Task[]>
+  }
 
   newTask(): void {
     const dialogRef = this.dialog.open(TaskDialogComponent, {
       width: `${270 / 16}rem`,
       data: {
-        task: {}
-      }
-    });
-
-    dialogRef.afterClosed().subscribe((result: TaskDialogResult | undefined) => {
-      if (!result) return
-
-      this.todo.push(result.task)
+        task: {},
+      },
     })
+
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) return
+
+        const addCollection = collection(this.store, 'todo')
+        addDoc(addCollection, result.task)
+      })
   }
 
   editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
@@ -46,30 +64,55 @@ export class AppComponent {
       width: `${270 / 16}rem`,
       data: {
         task,
-        enableDelete: true
-      }
+        enableDelete: true,
+      },
     })
 
-    dialogRef.afterClosed().subscribe((result: TaskDialogResult | undefined) => {
-      if (!result) return
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) return
 
-      const dataList = this[list]
-      const taskIndex = dataList.indexOf(task)
+        if (result.delete) {
+          const deleteCollection = collection(this.store, list)
+          const deleteItem = doc(deleteCollection, task.id)
 
-      if (result.delete) {
-        dataList.splice(taskIndex, 1)
-      } else {
-        dataList[taskIndex] = task
-      }
-    })
+          deleteDoc(deleteItem)
+        } else {
+          const updateCollection = collection(this.store, list)
+          const updateItem = doc(updateCollection, task.id)
+
+          updateDoc(updateItem, { ...task })
+        }
+      })
   }
 
-  drop(event: CdkDragDrop<Task[]>): void {
+  drop(event: CdkDragDrop<Task[] | null>): void {
     if (!event.container.data || !event.previousContainer.data) return
 
     if (event.previousContainer === event.container) {
-      return moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
+      return moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      )
     }
+
+    const item = event.previousContainer.data[event.previousIndex]
+
+    runTransaction(this.store, () => {
+      const deleteCollection = collection(this.store, event.previousContainer.id)
+      const deleteItem = doc(deleteCollection, item.id)
+
+      const addCollection = collection(this.store, event.container.id)
+
+      const promise = Promise.all([
+        deleteDoc(deleteItem),
+        addDoc(addCollection, item)
+      ])
+
+      return promise
+    })
 
     transferArrayItem(
       event.previousContainer.data,
